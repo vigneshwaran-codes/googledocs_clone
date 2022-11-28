d11cf7a3a9f9fa22abd31bdc6a4462db1cc1f31f
vigneshwarank00 <smilevicky2000@gmail.com>
2022-11-28 22:38:12 +0530

    removed dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import TextEditor from './components/TextEditor'
-import Dashboard from './components/Dashboard'
 import Home from './components/Home'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid'
